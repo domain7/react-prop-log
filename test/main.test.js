f4bb29788b58bd61bee96f715b76383eb286c612
Joe Flynn <joe@domain7.com>
2018-07-27 11:25:52 -0700

    add tests

--- test/main.test.js
+++ test/main.test.js
@@ -1,7 +1,41 @@
 import whatsNew from '../src/main';
 
 describe('whatsNew', () => {
-  it('should exist', () => {
-    expect(whatsNew).toBeTruthy();
+  beforeEach(() => {
+    global.console = { log: jest.fn() };
+  });
+
+  it('should log changed props', () => {
+    const prevProps = {
+      a: 1,
+      b: 2,
+      c: 3,
+    };
+    const component = {
+      constructor: { name: 'TestComponent' },
+      props: {
+        a: 4,
+        b: 2,
+        c: 3,
+      },
+    };
+
+    whatsNew(prevProps, component);
+    expect(console.log).toBeCalledWith('TestComponent:', 'a', 'changed from', 1, 'to', 4);
+  });
+
+  it('should not log if no props have changed', () => {
+    const prevProps = {
+      a: 1,
+      b: 2,
+      c: 3,
+    };
+    const component = {
+      constructor: { name: 'TestComponent' },
+      props: prevProps,
+    };
+
+    whatsNew(prevProps, component);
+    expect(console.log).not.toBeCalled();
   });
 });
